01d12cb9273dcc1a367291edee9f9bf1cc90947a
Cape23180909 <samilgamer@outlook.com>
2025-05-09 00:42:14 -0400

    Adactando el main para que ejecute el List en el mismo

--- app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
@@ -1,59 +1,73 @@
 package edu.ucne.registrotecnicos
 
+import TecnicoScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.room.Room
-import edu.ucne.registrotecnicos.data.database.TecnicoDb
+import androidx.lifecycle.viewmodel.compose.viewModel
+import edu.ucne.registrotecnicos.data.entities.TecnicoEntity
+import edu.ucne.registrotecnicos.presentation.TecnicoListScreen
+import edu.ucne.registrotecnicos.presentation.TecnicoViewModel
 import edu.ucne.registrotecnicos.ui.theme.RegistroTecnicosTheme
 
 class MainActivity : ComponentActivity() {
-    private lateinit var tecnicoDb: TecnicoDb
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
-        tecnicoDb = Room.databaseBuilder(
-            applicationContext,
-            TecnicoDb::class.java,
-            name = "Tecnico.db"
-        ).fallbackToDestructiveMigrationFrom()
-            .build()
-
-
         setContent {
-            RegistroTecnicosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            TecnicosListScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TecnicosListScreen(tecnicoViewModel: TecnicoViewModel = viewModel()) {
+    var Formulario by remember { mutableStateOf(false) }
+    var tecnicoToEdit by remember { mutableStateOf<TecnicoEntity?>(null) }
+    if (Formulario) {
+        TecnicoScreen(
+            tecnico = tecnicoToEdit,
+            agregarTecnico = { nombre, sueldo ->
+                if (tecnicoToEdit == null) {
+                    tecnicoViewModel.agregarTecnico(nombre, sueldo)
+                } else {
+                    tecnicoViewModel.updateTecnico(
+                        tecnicoToEdit!!.copy(Nombre = nombre, Sueldo = sueldo)
+                    )
+                }
+                Formulario = false
+                tecnicoToEdit = null
+            },
+            onCancel = {
+                Formulario = false
+                tecnicoToEdit = null
+            }
+        )
+    } else {
+        TecnicoListScreen(
+            tecnicoList = tecnicoViewModel.tecnicoList,
+            onCreate = {
+                tecnicoToEdit = null
+                Formulario = true
+            },
+            onDelete = { tecnicoViewModel.deleteTecnico(it) },
+            onEdit = {
+                tecnicoToEdit = it
+                Formulario = true
+            }
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     RegistroTecnicosTheme {
-        Greeting("Android")
+        TecnicosListScreen()
     }
 }
