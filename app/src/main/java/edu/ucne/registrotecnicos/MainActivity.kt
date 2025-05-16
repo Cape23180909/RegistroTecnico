6ce423b24143201dc307575621a7b4f321191346
Cape23180909 <samilgamer@outlook.com>
2025-05-16 01:30:35 -0400

    Colocando este codigo mas optimno, y poniendo el dicho proyecto mas organizado el mismo.

--- app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
@@ -1,73 +1,46 @@
 package edu.ucne.registrotecnicos
 
-import TecnicoScreen
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
-import edu.ucne.registrotecnicos.data.entities.TecnicoEntity
-import edu.ucne.registrotecnicos.presentation.TecnicoListScreen
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import edu.ucne.registrotecnicos.data.database.TecnicoDb
+import edu.ucne.registrotecnicos.data.repository.TecnicoRepository
 import edu.ucne.registrotecnicos.presentation.TecnicoViewModel
+import edu.ucne.registrotecnicos.presentation.navigation.TecnicosNavHost
 import edu.ucne.registrotecnicos.ui.theme.RegistroTecnicosTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var tecnicoDb: TecnicoDb
+    private lateinit var tecnicosRepository: TecnicoRepository
+    private lateinit var tecnicosViewModel: TecnicoViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // Inicialización de la base de datos
+        tecnicoDb = Room.databaseBuilder(
+            applicationContext,
+            TecnicoDb::class.java,
+            "Tecnico.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+        tecnicosRepository = TecnicoRepository(tecnicoDb.TecnicoDao())
+        tecnicosViewModel = TecnicoViewModel(tecnicosRepository)
+
         setContent {
-            TecnicosListScreen()
+            RegistroTecnicosTheme {
+                val navController = rememberNavController()
+                TecnicosNavHost(
+                    navHostController = navController,
+                    viewModel = tecnicosViewModel
+                )
+            }
         }
-    }
-}
-
-@Composable
-fun TecnicosListScreen(tecnicoViewModel: TecnicoViewModel = viewModel()) {
-    var Formulario by remember { mutableStateOf(false) }
-    var tecnicoToEdit by remember { mutableStateOf<TecnicoEntity?>(null) }
-    if (Formulario) {
-        TecnicoScreen(
-            tecnico = tecnicoToEdit,
-            agregarTecnico = { nombre, sueldo ->
-                if (tecnicoToEdit == null) {
-                    tecnicoViewModel.agregarTecnico(nombre, sueldo)
-                } else {
-                    tecnicoViewModel.updateTecnico(
-                        tecnicoToEdit!!.copy(Nombre = nombre, Sueldo = sueldo)
-                    )
-                }
-                Formulario = false
-                tecnicoToEdit = null
-            },
-            onCancel = {
-                Formulario = false
-                tecnicoToEdit = null
-            }
-        )
-    } else {
-        TecnicoListScreen(
-            tecnicoList = tecnicoViewModel.tecnicoList,
-            onCreate = {
-                tecnicoToEdit = null
-                Formulario = true
-            },
-            onDelete = { tecnicoViewModel.deleteTecnico(it) },
-            onEdit = {
-                tecnicoToEdit = it
-                Formulario = true
-            }
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RegistroTecnicosTheme {
-        TecnicosListScreen()
     }
 }
