c16a96c6cc9debd9e19e56a3c027cd169824aeb8
Cape23180909 <samilgamer@outlook.com>
2025-05-16 01:21:07 -0400

    Removiendo Imports Inceserarios.

--- app/src/main/java/edu/ucne/registrotecnicos/data/dao/TecnicoDao.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/dao/TecnicoDao.kt
@@ -2,6 +2,7 @@ package edu.ucne.registrotecnicos.data.dao
 
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Upsert
 import edu.ucne.registrotecnicos.data.entities.TecnicoEntity
