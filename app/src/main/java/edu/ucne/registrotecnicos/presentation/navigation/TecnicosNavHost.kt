ebc2e554eb33d82a7c3c951c4b6d1d50be3f5a12
Cape23180909 <samilgamer@outlook.com>
2025-05-16 01:24:34 -0400

    Creando NavHost de tecnicos

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/TecnicosNavHost.kt
@@ -0,0 +1,59 @@
+package edu.ucne.registrotecnicos.presentation.navigation
+
+import TecnicoScreen
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import edu.ucne.registrotecnicos.presentation.TecnicoListScreen
+import edu.ucne.registrotecnicos.presentation.TecnicoViewModel
+
+@Composable
+fun TecnicosNavHost(
+    navHostController: NavHostController,
+    viewModel: TecnicoViewModel
+) {
+    NavHost(
+        navController = navHostController,
+        startDestination = Screen.TecnicoList
+    ) {
+        composable<Screen.TecnicoList> {
+            val tecnicoList = viewModel.tecnicoList.collectAsState().value
+
+            TecnicoListScreen(
+                tecnicoList = tecnicoList,
+                onEdit = { tecnico ->
+                    navHostController.navigate(Screen.Tecnico(tecnico.TecnicoId))
+                },
+                onCreate = {
+                    navHostController.navigate(Screen.Tecnico(null))
+                },
+                onDelete = { tecnico ->
+                    viewModel.delete(tecnico)
+                }
+            )
+        }
+
+        composable<Screen.Tecnico> { backStackEntry ->
+            val tecnicoId = backStackEntry.toRoute<Screen.Tecnico>().tecnicoId
+            val tecnico = viewModel.getTecnicoById(tecnicoId)
+
+            TecnicoScreen(
+                tecnico = tecnico,
+                agregarTecnico = { nombre, sueldo ->
+                    if (tecnico == null) {
+                        viewModel.agregarTecnico(nombre, sueldo)
+                    } else {
+                        viewModel.update(tecnico.copy(Nombre = nombre, Sueldo = sueldo))
+                    }
+                    navHostController.popBackStack()
+                },
+                onCancel = {
+                    navHostController.popBackStack()
+                }
+            )
+        }
+    }
+}
