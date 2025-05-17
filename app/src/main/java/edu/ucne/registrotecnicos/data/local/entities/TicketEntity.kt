fdf22af0edd13f7a43c2e51b0cd1c955811e7e34
Cape23180909 <samilgamer@outlook.com>
2025-05-17 00:29:54 -0400

    Terminando de hacer el TicketEntity

---
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/entities/TicketEntity.kt
@@ -0,0 +1,14 @@
+package edu.ucne.registrotecnicos.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Tickets")
+data class TicketEntity(
+    @PrimaryKey(autoGenerate = true)
+    val TicketId: Int?=null,
+    val Fecha: String= "",
+    val Cliente: String= "",
+    val Asunto: String= "",
+    val Descripcion: String= "",
+    )
