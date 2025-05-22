d077c96952f6b8b73af2ff9923bffe3f7dd1626f
Cape23180909 <samilgamer@outlook.com>
2025-05-22 15:06:54 -0400

    Terminando de hacer el repository de mensaje.

---
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/repository/MensajeRepository.kt
@@ -0,0 +1,26 @@
+package ucne.edu.registrotecnicos.data.repository
+
+import edu.ucne.registrotecnicos.data.local.database.TareaDb
+import edu.ucne.registrotecnicos.data.local.entities.MensajeEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class MensajeRepository @Inject constructor(
+    private val tareaDb: TareaDb
+) {
+    suspend fun save(mensaje: MensajeEntity) {
+        tareaDb.MensajeDao().save(mensaje)
+    }
+
+    suspend fun find(id: Int): MensajeEntity? {
+        return tareaDb.MensajeDao().find(id)
+    }
+
+    suspend fun delete(mensaje: MensajeEntity) {
+        tareaDb.MensajeDao().delete(mensaje)
+    }
+
+    fun getAll(): Flow<List<MensajeEntity>> {
+        return tareaDb.MensajeDao().getAll()
+    }
+}
