4f57b182dd634880edcb5e4860a2c30c9f84a38f
Cape23180909 <samilgamer@outlook.com>
2025-05-22 15:05:55 -0400

    Actualizando la version de dicha base de datos a la version 4.

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/database/TareaDb.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/database/TareaDb.kt
@@ -2,20 +2,24 @@ package edu.ucne.registrotecnicos.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.registrotecnicos.data.local.dao.MensajeDao
 import edu.ucne.registrotecnicos.data.local.dao.TecnicoDao
 import edu.ucne.registrotecnicos.data.local.dao.TicketDao
+import edu.ucne.registrotecnicos.data.local.entities.MensajeEntity
 import edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
 import edu.ucne.registrotecnicos.data.local.entities.TicketEntity
 
 @Database(
     entities = [
         TecnicoEntity::class,
-        TicketEntity::class
+        TicketEntity::class,
+        MensajeEntity::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class TareaDb : RoomDatabase() {
     abstract fun TecnicoDao(): TecnicoDao
     abstract fun TicketDao(): TicketDao
+    abstract fun MensajeDao(): MensajeDao
 }
