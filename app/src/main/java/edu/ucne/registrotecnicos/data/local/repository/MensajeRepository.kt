de7f1f23f1f26c1352ee3c376aac6687a3c407f5
Cape23180909 <samilgamer@outlook.com>
2025-06-13 11:16:58 -0400

    Adactando este repository, hilt

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/repository/MensajeRepository.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/repository/MensajeRepository.kt
@@ -1,5 +1,6 @@
 package ucne.edu.registrotecnicos.data.repository
 
+import edu.ucne.registrotecnicos.data.local.dao.MensajeDao
 import edu.ucne.registrotecnicos.data.local.database.TareaDb
 import edu.ucne.registrotecnicos.data.local.entities.MensajeEntity
 import kotlinx.coroutines.flow.Flow
@@ -7,20 +8,20 @@ import javax.inject.Inject
 
 class MensajeRepository @Inject constructor(
     private val tareaDb: TareaDb
-) {
-    suspend fun save(mensaje: MensajeEntity) {
+){
+    suspend fun saveMensaje(mensaje: MensajeEntity){
         tareaDb.MensajeDao().save(mensaje)
     }
 
-    suspend fun find(id: Int): MensajeEntity? {
+    suspend fun find(id: Int): MensajeEntity?{
         return tareaDb.MensajeDao().find(id)
     }
 
-    suspend fun delete(mensaje: MensajeEntity) {
-        tareaDb.MensajeDao().delete(mensaje)
+    suspend fun delete(mensaje: MensajeEntity){
+        return tareaDb.MensajeDao().delete(mensaje)
     }
 
-    fun getAll(): Flow<List<MensajeEntity>> {
+    suspend fun getAll(): Flow<List<MensajeEntity>>{
         return tareaDb.MensajeDao().getAll()
     }
 }
