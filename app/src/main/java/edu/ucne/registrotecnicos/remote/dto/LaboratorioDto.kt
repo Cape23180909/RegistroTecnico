f145b91373b81b85109fb3c8709caa6930e7a562
Cape23180909 <samilgamer@outlook.com>
2025-06-13 21:44:45 -0400

    Depurando el Dto

--- app/src/main/java/edu/ucne/registrotecnicos/remote/dto/LaboratorioDto.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/remote/dto/LaboratorioDto.kt
@@ -1,7 +1,7 @@
 package edu.ucne.registrotecnicos.remote.dto
 
 data class LaboratorioDto(
-    val laboratorioId: Int,
+    val laboratorioId: Int = 0,
     val descripcion: String,
     val monto: Double
 )
