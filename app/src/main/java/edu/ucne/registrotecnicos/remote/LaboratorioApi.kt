6e427f8a53432631ddde08a523ebc56b87b70528
Cape23180909 <samilgamer@outlook.com>
2025-06-13 21:44:18 -0400

    Colocando la palabra "Laboratorios" plural, para que asi se comunique bien la api sin fallos.

--- app/src/main/java/edu/ucne/registrotecnicos/remote/LaboratorioApi.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/remote/LaboratorioApi.kt
@@ -1,22 +1,21 @@
-package edu.ucne.registrotecnicos.remote
+package edu.ucne.composedemo.data.remote
 
 import edu.ucne.registrotecnicos.remote.dto.LaboratorioDto
 import retrofit2.http.*
 
-interface LaboratorioApi{
-
-    @GET("api/laboratorios")
+interface LaboratorioApi {
+    @GET("api/Laboratorios")
     suspend fun getLaboratorios(): List<LaboratorioDto>
 
-    @GET("api/laboratorios/{id}")
+    @GET("api/Laboratorios/{id}")
     suspend fun getLaboratorio(@Path("id") id: Int): LaboratorioDto
 
-    @POST("api/laboratorios")
-    suspend fun createLaboratorio(@Body laboratorio: LaboratorioDto): LaboratorioDto
+    @POST("api/Laboratorios")
+    suspend fun createLaboratorio(@Body laboratorioDto: LaboratorioDto): LaboratorioDto
 
-    @PUT("api/laboratorios/{id}")
-    suspend fun updateLaboratorio(@Path("id") id: Int, @Body laboratorio: LaboratorioDto): LaboratorioDto
+    @PUT("api/Laboratorios/{id}")
+    suspend fun updateLaboratorio(@Path("id") id: Int, @Body laboratorioDto: LaboratorioDto): LaboratorioDto
 
-    @DELETE("api/laboratorios/{id}")
+    @DELETE("api/Laboratorios/{id}")
     suspend fun deleteLaboratorio(@Path("id") id: Int)
 }
